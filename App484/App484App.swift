d7a8a5d1a83bdb34effacd6458fc5811963f21ed
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-10-30 20:20:29 +0300

    Initial Commit

---
+++ App484/App484App.swift
@@ -0,0 +1,17 @@
+//
+//  App484App.swift
+//  App484
+//
+//  Created by IGOR on 30/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct App484App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
